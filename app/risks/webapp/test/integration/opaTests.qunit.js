83710f4078b3739fd21c70b530d53d1447e46a45
Arf Mahamud <fmarif2009@gmail.com>
2023-11-25 17:49:44 +0700

    Risk-Mitigations

---
+++ app/risks/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'np/risks/test/integration/FirstJourney',
+		'np/risks/test/integration/pages/RisksList',
+		'np/risks/test/integration/pages/RisksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, RisksList, RisksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('np/risks') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheRisksList: RisksList,
+					onTheRisksObjectPage: RisksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
